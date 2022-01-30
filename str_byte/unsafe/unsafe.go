d030e507cc91f56f169a0c325f66711ea8f3ee0a
sumengliang <sumengliang@didiglobal.com>
2022-01-30 15:14:25 +0800

    update Str2byte

--- str_byte/unsafe/unsafe.go
+++ str_byte/unsafe/unsafe.go
@@ -1,6 +1,9 @@
 package unsafe
 
-import "unsafe"
+import (
+	"reflect"
+	"unsafe"
+)
 
 //// https://github.com/golang/go/blob/master/src/runtime/slice.go#L15
 //type slice struct {
@@ -15,10 +18,19 @@ import "unsafe"
 //	len int
 //}
 
-func Str2byte(str string) []byte {
+func str2byte(str string) []byte {
 	tmp := (*[2]uintptr)(unsafe.Pointer(&str))
 	bs := [3]uintptr{tmp[0], tmp[1], tmp[1]}
 	return *(*[]byte)(unsafe.Pointer(&bs))
+}
+
+func Str2byte(str string) (bs []byte) {
+	strHdr := (*reflect.StringHeader)(unsafe.Pointer(&str))
+	sliceHdr := (*reflect.SliceHeader)(unsafe.Pointer(&bs))
+	sliceHdr.Data = strHdr.Data
+	sliceHdr.Cap = strHdr.Len
+	sliceHdr.Len = strHdr.Len
+	return
 }
 
 func Byte2str(bt []byte) string {
